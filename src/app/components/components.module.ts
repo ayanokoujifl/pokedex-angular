fa0fe58d19a1ffd09e0e1943258cc3c4e9b61baa
Ayanokoujifl <guleite3@gmail.com>
2023-04-25 23:09:43 -0300

    main components for home page

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SearchComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HeaderComponent,
+    SearchComponent,
+    ListComponent
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { DetailsComponent } from './details/details.component'
+import { ComponentsModule } from '../components/components.module'
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    ComponentsModule
+  ]
+})
+export class PagesModule { }
